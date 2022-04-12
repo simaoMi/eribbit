ff4f3c77d3c61c103acbd03de4ca396bff8eec92
Misimao <1422737964@qq.com>
2022-04-12 16:40:10 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
+<!--
+ * @Author: DM
+ * @Email: simaoMi_DM@163.com
+ * @Description: xxxx
+ * @Date: 2022-04-12 15:59:24
+ * @LastEditTime: 2022-04-12 16:25:15
+ * @Reference: xxxx
+-->
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>APP</div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  name: 'App'
 }
+</script>
 
-nav {
-  padding: 30px;
+<style lang="less" scoped>
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: DM
+ * @Email: simaoMi_DM@163.com
+ * @Description: xxx
+ * @Date: 2022-04-12 15:59:24
+ * @LastEditTime: 2022-04-12 16:36:56
+ * @Reference: xxx
+ */
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
